365c31c91a42ebf4ede958c1cdfb224d253407c8
etiennedub <you@example.com>
2016-09-21 09:23:29 -0400

    Implemantation arret

--- TP1/station.cpp
+++ TP1/station.cpp
@@ -5,6 +5,13 @@
  *      Author: etudiant
  */
 
-
-
-
+#include "station.h"
+/*
+Station::Station(const std::vector<std::string>& ligne_gtfs){
+    m_id = std::stoi(ligne_gtfs[0]);
+    m_nom = ligne_gtfs[1];
+    m_description = ligne_gtfs[3];
+    // Coordonnees m_coords(std::stod(ligne_gtfs[3]),std::stod(ligne_gtfs[4]));
+    m_voyages_passants = {};
+}
+*/

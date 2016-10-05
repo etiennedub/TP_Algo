4c0a38cf9c055ddef192eb80dd8ee140eb307658
etiennedub <you@example.com>
2016-10-04 22:40:23 -0400

    main non fini

--- TP1/main.cpp
+++ TP1/main.cpp
@@ -8,6 +8,7 @@
 #include "ligne.h"
 #include <iostream>
 #include <ctime>
+#include <unordered_map>
 
 void testVoyage();
 int main(){
@@ -23,25 +24,42 @@ std::vector<std::vector<std::string>> lireFichier(std::string path){
 }
 
 void testVoyage(){
-	std::vector<std::vector<std::string>> file;
-	file = lireFichier("RTC/routes.txt");
-	Ligne ligneObj(file[0]);
+	std::vector<std::vector<std::string>> fichierLigne;
+	std::vector<std::vector<std::string>> fichierArret;
+	std::vector<std::vector<std::string>> fichierVoyage;
+
+	std::unordered_map <std::string, std::vector<Arret>> mapArret;
+	std::unordered_map <std::string, Ligne> mapLigne;
+	std::unordered_map <unsigned int, Voyage> mapVoyage;
 
 
-	file = lireFichier("RTC/stop_times.txt");
-	std::vector<Arret> arretVec;
-	for (int i = 0; i < 10; i++){
-		Arret arretObj(file[i]);
-		arretVec.push_back(arretObj);
+
+	fichierArret = lireFichier("RTC/stop_times.txt");
+	for (int i = 0; i < fichierArret.size(); i++){
+		  std::unordered_map<std::string, std::vector<Arret>>::const_iterator it = mapArret.find (fichierArret[i][0]);
+		if(it != mapArret.end()){
+			mapArret[fichierArret[i][0]].push_back(Arret(fichierArret[i]));
+		}
+		else{
+			std::vector<Arret> tempo = {Arret(fichierArret[i])};
+			mapArret.insert({fichierArret[i][0], tempo});
+		}
 	}
 
-	file = lireFichier("RTC/trips.txt");
-	std::vector<std::string> test = file[75];
-	Voyage voyageObj(test, &ligneObj);
 
-	voyageObj.setArrets(arretVec);
+	fichierLigne = lireFichier("RTC/routes.txt");
+	for (int i = 0; i < fichierLigne.size(); i++){
+			mapLigne.insert({fichierLigne[i][0], Ligne(fichierLigne[i])});
+	}
 
-	std::cout << voyageObj;
+	fichierVoyage = lireFichier("RTC/trips.txt");
+		for (unsigned int i = 0; i < fichierVoyage.size(); i++){
+			std::vector<Ligne> tempoLigne = {Ligne(fichierLigne[0]), Ligne(fichierLigne[1])};
+			Voyage tempo = Voyage(fichierVoyage[i], &tempoLigne[1]);
+			tempo.setArrets(mapArret[fichierVoyage[i][1]]);
+			mapVoyage.insert({i, tempo});
+	}
+
 /*
 	std::cout << "Ceci est un test \n";
 	for (int i = 0; i < ligneVec.size(); i++){

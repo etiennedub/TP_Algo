bce3b7e39773650d701b71aee9db5d40bcab3260
etienned <etiennedubeau@hotmail.com>
2016-11-03 13:27:27 -0400

    Fix compilation gestionnaire

--- TP2/gestionnaire.cpp
+++ TP2/gestionnaire.cpp
@@ -3,70 +3,45 @@
 //
 #include "gestionnaire.h"
 
-Gestionnaire::Gestionnaire(std::string chemin_dossier){
+Gestionnaire::Gestionnaire(std::string chemin_dossier) {
+//    // expemple lecture fichier
+//    std::string file_p = path;
+//    std::vector<std::vector<std::string>> resultats;
+//    lireFichier(file_p, resultats, ',' , true);
 
+    std::vector<std::vector<std::string>> fichierRoutes;
+    std::vector<std::vector<std::string>> fichierTrips;
+    std::vector<std::vector<std::string>> fichierStopTimes;
+    std::vector<std::vector<std::string>> fichierStops;
+    std::vector<std::vector<std::string>> fichierCalendarDates;
 
+    lireFichier(chemin_dossier + "/routes.txt", fichierRoutes, ',', true);
+    lireFichier(chemin_dossier + "/trips.txt", fichierTrips, ',', true);
+    lireFichier(chemin_dossier + "/stop_times.txt", fichierStopTimes, ',', true);
+    lireFichier(chemin_dossier + "/stops.txt", fichierStops, ',', true);
+    lireFichier(chemin_dossier + "/calendar_dates.txt", fichierCalendarDates, ',', true);
+
+    // Creation objets station
+    for (int i = 0; i < fichierStops.size(); i++) {
+        Station station = Station(fichierStops[i]);
+        // mapStation.insert({tempo.getId(), tempo});
+    }
 }
 
 bool Gestionnaire::date_est_prise_en_charge(const Date& date){
-	std::unordered_map<std::string,int>::iterator it;
-	for(it = m_voyages.begin(); it != m_voyages.end(); it++){
-		if(m_voyages_date.count(date)>0)
-			return true;
-		else
-			return false;
-	}
+		//return (m_voyages_date.count(date) > 0);
 }
 
 bool Gestionnaire::bus_existe(std::string num_ligne){
-	std::unordered_map<std::string,int>::iterator it;
-	for(it = m_lignes.begin(); it != m_lignes.end(); it++){
-		if(m_lignes.count(num_ligne)>0)
-			return true;
-		else
-			return false;
-	}
+		return (m_lignes.count(num_ligne) > 0);
 }
 
 bool Gestionnaire::station_existe(int station_id){
-	std::unordered_map<std::string,int>::iterator it;
-	for(it = m_stations.begin(); it != m_stations.end(); it++){
-		if(m_stations.count(station_id)>0)
-			return true;
-		else
-			return false;
-	}
+
+		return (m_stations.count(station_id) >0 );
 }
 
 Ligne Gestionnaire::getLigne(std::string num_ligne){
 
 }
 
-Station Gestionnaire::getStation(int station_id){
-
-}
-
-std::pair<std::string, std::string> get_bus_destinations(int station_id, std::string num_ligne){
-
-}
-
-std::vector<std::pair<double, Station*>> trouver_stations_environnantes(Coordonnees coord, double rayon){
-
-}
-
-std::vector<Heure> trouver_horaire(Date date, Heure heure, std::string numero_ligne, int station_id, std::string destination){
-
-}
-
-/*bool Gestionnaire::reseau_est_fortement_connexe(Date date, Heure heure_debut, bool considerer_transfert=true){
-
-}
-
-void Gestionnaire::composantes_fortement_connexes(Date date, Heure heure_debut,
-			std::vector< std::vector<unsigned int> >& composantes, bool considerer_transfert=true){
-
-}*/
-
-std::vector< unsigned int > plus_court_chemin(Date date, Heure heure_depart, Coordonnees depart, Coordonnees destination){
-
-}

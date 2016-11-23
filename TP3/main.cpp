fb5cb7d352652438ee6e15585a94d215cdb37b96
etienned <etiennedubeau@hotmail.com>
2016-11-23 09:52:48 -0500

    Supression de gestionnaire

--- TP3/main.cpp
+++ TP3/main.cpp
@@ -11,8 +11,8 @@ using namespace std;
 int main() {
     GestionnaireInvestigation rtc("RTC");
 
-    cout << "Temps moyen de dijsktra " << rtc.tester_n_paires_dijsktra(1000) << endl;
+    cout << "Temps moyen de dijsktra " << rtc.tester_n_paires_dijsktra(100) << endl;
 
-    cout << "Temps moyen de bellman " << rtc.tester_n_paires_bellman(1000) << endl;
+    cout << "Temps moyen de bellman " << rtc.tester_n_paires_bellman(100) << endl;
     return 0;
 }
